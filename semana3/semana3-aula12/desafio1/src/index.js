39e38b5b335d7c88f3c88f0a1568e30c8bf761a3
vitorfarias001 <contato.vitorfariass@gmail.com>
2020-10-01 12:48:15 -0300

    exercicos-desafios-finalizados

---
+++ semana3/semana3-aula12/desafio1/src/index.js
@@ -0,0 +1,7 @@
+//--- DESAFIO 1 ---/
+//O resultado impresso será:
+//0
+//00
+//000
+//0000
+// 
---
+++ semana3/semana3-aula12/exercicio3/src/index.js
@@ -0,0 +1,43 @@
+//Exercício 3
+
+const array = [13, 16, 12, 76, 84, 23, 10]
+//a.
+    // for (array) {
+    //     console.log(i)
+    // }
+
+//b.    
+    // for (array) {
+    //     console.log(i / 10)
+    // }
+
+//c.
+//     for (let elementArray of array) {
+//         if (elementArray % 2 === 0) {
+//         console.log(elementArray)
+//     }
+//  }    
+
+//d.
+    // const novoArray = []
+    // let index = 0
+    // for (array) {
+    // novoArray.push("O elemento é: " + i + "e o index é: " + index)
+    // index++
+    // }
+    // console.log(novoArray)
+
+//e.
+//     let valorMaximo = 0
+//     for (let iMax of array) {
+//         if (iMax > valorMaximo) {
+//             valorMaximo = iMax
+//   }
+// }
+//     let valorMinimo = valorMaximo
+//     for (let minimo of array) {
+//         if (minimo < valorMinimo) {
+//             valorMinimo = minimo 
+//   }
+// }
+//     console.log ("O maior número é " + valorMaximo + " e o menor é " + valorMinimo)
